c7ffa7cd0cdd724aa60569e278460ceb86da69f2
olcio45 <olcio45@gmail.com>
2018-05-15 22:20:24 +0200

    style css

--- js/js.js
+++ js/js.js
@@ -1,5 +1,11 @@
 const show = document.querySelector('.nav-dropdown-link');
 const menu = document.querySelector('.nav-dropdown-menu');
 show.addEventListener('click', function () {
-    menu.classList.toggle('show');
+        menu.classList.toggle('show');
+});
+
+window.addEventListener('click', function (e) {
+    if (e.target!==show) {
+        menu.classList.remove('show');
+    }
 });
