03d31e2d9bca47506c4bde85a131357627ccc011
olcio45 <olcio45@gmail.com>
2018-05-16 21:09:02 +0200

    search button

--- js/js.js
+++ js/js.js
@@ -1,7 +1,9 @@
 const show = document.querySelector('.nav-dropdown-link');
 const menu = document.querySelector('.nav-dropdown-menu');
-show.addEventListener('click', function () {
-        menu.classList.toggle('show');
+
+show.addEventListener('click', function (e) {
+    e.preventDefault();
+    menu.classList.toggle('show');
 });
 
 window.addEventListener('click', function (e) {
@@ -9,3 +11,41 @@ window.addEventListener('click', function (e) {
         menu.classList.remove('show');
     }
 });
+
+const container = document.querySelector('.container');
+const page = document.querySelectorAll('.section-main-container');
+const form = document.querySelector('.nav-form');
+const input = document.querySelector('.nav-form-search');
+const logo = document.querySelector('.nav-link');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let p = '';
+    for (let i=0; i<page.length; i++) {
+        p = page[i].querySelectorAll('p')[0];
+
+        let filter = input.value.toUpperCase();
+
+        if (p.innerText.toUpperCase().indexOf(filter) > -1) {
+            page[i].classList.add('show');
+
+        } else {
+            page[i].classList.add('hide');
+            page[i].classList.remove('show');
+            container.classList.add('hide');
+        }
+    }
+
+});
+
+logo.addEventListener('click', function () {
+    container.classList.remove('hide');
+    let p = '';
+    for (let i=0; i<page.length; i++) {
+        p = page[i].querySelectorAll('p')[0];
+        page[i].classList.remove('show');
+        page[i].classList.remove('hide');
+    }
+
+});
+
